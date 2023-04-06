399d9d66d36ece32a2086e8df2ae8f12878029a8
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-04-06 13:50:47 +0200

    number Bar

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+// create list of numbers until the page ends
+
+const ul = document.getElementById("numbers");
+
+function createNumber(i) {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(i.toString()));
+    ul.appendChild(li);
+}
+
+// px to em
+
+$.fn.toEm = function(settings){
+    settings = jQuery.extend({
+        scope: 'body'
+    }, settings);
+    var that = parseInt(this[0],10),
+        scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+        scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return (that / scopeVal).toFixed(8) + 'em';
+};
+
+
+$.fn.toPx = function(settings){
+    settings = jQuery.extend({
+        scope: 'body'
+    }, settings);
+    var that = parseFloat(this[0]),
+        scopeTest = jQuery('<div style="display: none; font-size: 1em; margin: 0; padding:0; height: auto; line-height: 1; border:0;">&nbsp;</div>').appendTo(settings.scope),
+        scopeVal = scopeTest.height();
+    scopeTest.remove();
+    return Math.round(that * scopeVal) + 'px';
+};
+
+
+//get how many lines there are
+
+var x = document.getElementById("content").clientHeight; 
+console.log(x);
+var xEm = $(x).toEm();
+var lines = Math.round(parseFloat(xEm.replace('em', ''))/1.4)-1;
+console.log(lines);
+
+
+
+for (let i = 0; i < lines; i++) {
+    createNumber(i+1);
+}
