4b70a4bf0d829a92817c01b122c2a8e3969f2161
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-04-08 21:24:57 +0200

    mobile phone fix

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
-const ul = document.getElementById("numbers");
-
+// function to create a list item for each line of content
 function createNumber(i) {
-    var li = document.createElement("li");
+    const li = document.createElement("li");
     li.appendChild(document.createTextNode(i.toString()));
-    ul.appendChild(li);
-}
-
-// create temporary li element to get its height
-var liTemp = document.createElement("li");
-liTemp.innerHTML = "0";
-ul.appendChild(liTemp);
-var liHeight = liTemp.offsetHeight;
-ul.removeChild(liTemp);
-
-var contentHeight = document.getElementById("content").clientHeight;
-var numLines = Math.ceil(contentHeight / liHeight);
-
-for (let i = 0; i < numLines; i++) {
-    createNumber(i+1);
-}
+    document.getElementById("numbers").appendChild(li);
+  }
+  
+  // function to remove all list items from the numbers list
+  function clearNumbers() {
+    const numbers = document.getElementById("numbers");
+    while (numbers.firstChild) {
+      numbers.removeChild(numbers.firstChild);
+    }
+  }
+  
+  // function to calculate and create the correct number of list items based on the content height
+  function updateNumbers() {
+    // calculate the number of lines of content based on its height
+    const content = document.getElementById("content");
+    const contentHeight = content.clientHeight;
+    const contentSizeVH = (100 * contentHeight) / window.innerHeight;
+    const numLines = Math.floor(contentSizeVH / 3.2);
+    
+    // remove any existing list items from the numbers list
+    clearNumbers();
+    
+    // create a list item for each line of content
+    for (let i = 1; i <= numLines; i++) {
+      createNumber(i);
+    }
+  }
+  
+  // create the initial list of numbers
+  updateNumbers();
+  
+  // add a resize event listener to update the list of numbers when the window is resized
+  window.addEventListener("resize", updateNumbers);
+  
