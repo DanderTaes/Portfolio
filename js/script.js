5a5613e0242545a7005f9c6afadc7c4a5d6aba84
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-04-08 21:51:49 +0200

    small2

--- js/script.js
+++ js/script.js
@@ -1,43 +1,55 @@
 // function to create a list item for each line of content
 function createNumber(i) {
-    const li = document.createElement("li");
-    li.appendChild(document.createTextNode(i.toString()));
-    document.getElementById("numbers").appendChild(li);
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(i.toString()));
+  document.getElementById("numbers").appendChild(li);
+}
+
+// function to remove all list items from the numbers list
+function clearNumbers() {
+  const numbers = document.getElementById("numbers");
+  while (numbers.firstChild) {
+    numbers.removeChild(numbers.firstChild);
   }
-  
-  // function to remove all list items from the numbers list
-  function clearNumbers() {
-    const numbers = document.getElementById("numbers");
-    while (numbers.firstChild) {
-      numbers.removeChild(numbers.firstChild);
-    }
+}
+
+// function to calculate and create the correct number of list items based on the content height
+function updateNumbers() {
+  // calculate the number of lines of content based on its height
+  var lineHeight = 3.2;
+  const content = document.getElementById("content");
+  const contentHeight = content.clientHeight;
+  const contentSizeVH = (100 * contentHeight) / window.innerHeight;
+  if (window.innerWidth <= 600) {
+    console.log("hello im small");
+    lineHeight = 2.5;
   }
-  
-  // function to calculate and create the correct number of list items based on the content height
-  function updateNumbers() {
-    // calculate the number of lines of content based on its height
-    var lineHeight = 3.2;
-    const content = document.getElementById("content");
-    const contentHeight = content.clientHeight;
-    const contentSizeVH = (100 * contentHeight) / window.innerHeight;
-    if (window.innerWidth <= 600){
-        console.log("hello im small")
-        lineHeight = 2.5    
-    }
-    const numLines = Math.floor(contentSizeVH / lineHeight);
-    
-    // remove any existing list items from the numbers list
-    clearNumbers();
-    
-    // create a list item for each line of content
-    for (let i = 1; i <= numLines; i++) {
-      createNumber(i);
-    }
+  const numLines = Math.floor(contentSizeVH / lineHeight);
+
+  // remove any existing list items from the numbers list
+  clearNumbers();
+
+  // create a list item for each line of content
+  for (let i = 1; i <= numLines; i++) {
+    createNumber(i);
   }
-  
-  // create the initial list of numbers
-  updateNumbers();
-  
-  // add a resize event listener to update the list of numbers when the window is resized
-  window.addEventListener("resize", updateNumbers);
-  
+}
+
+// create the initial list of numbers
+updateNumbers();
+
+// debounce function
+function debounce(func, delay) {
+  let timeout;
+  return function executedFunction(...args) {
+    const later = () => {
+      timeout = null;
+      func(...args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, delay);
+  };
+}
+
+// add a resize event listener to update the list of numbers when the window is resized
+window.addEventListener("resize", debounce(updateNumbers, 500));
