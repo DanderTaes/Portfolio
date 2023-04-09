760a24bf16c66d421c28d1718fcaac5a77b7508b
DanderTaes <73030752+DanderTaes@users.noreply.github.com>
2023-04-09 14:36:27 +0200

    coding integrated1

---
+++ js/change_page.js
@@ -0,0 +1,8 @@
+//detect when the run button is pressed
+
+function runCode() {
+    const code = document.getElementById("executableCode").value;
+    if (code != ""){
+        console.log(code)
+    }
+}
